8532193f7e87931547a3354c672b7526dd1c5453
Thomas-Lacomblez <34097025+Thomas-Lacomblez@users.noreply.github.com>
2022-01-10 15:00:55 +0100

    Update Launcher.java

--- src/fr/cnamts/videotech/launcher/Launcher.java
+++ src/fr/cnamts/videotech/launcher/Launcher.java
@@ -7,22 +7,10 @@ import fr.cnamts.gestioncompte.domaine.Compte;
 import fr.cnamts.gestioncompte.service.CompteService;
 
 public class Launcher {
+    public static void main(String[] args) {
+        Video v1 = new Video(2, "Ville nouvelle", "Documentaire", "3", "20/30/2020");
 
-	public static void main(String[] args) {
-
-		Compte compte1 = new Compte(1, 123456789, "BREIZH FOREVER", "04");
-
-		IDao iDao = new CompteDao();
-		IDao iDaoJpa = new CompteDaoJpa();
-		CompteService compteService = new CompteService(iDaoJpa);
-		compteService.creerCompte(compte1);
-
-		Compte compte2 = compteService.recuperationCompte(2);
-		if (compte2 != null) {
-			System.out.println("LAUNCHER: Récuperation du compte réussie " + compte2.toString());
-		} else {
-			System.out.println("LAUNCHER: Récuperation du compte échouée");
-		}
-	}
-
+        VideoService videoService = new VideoService();
+        videoService.creerVideo(v1);
+    }
 }
